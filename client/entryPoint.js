c6e4919ef3b4097a0fba7256b5d9d1c8cf625eaa
Kennie Davis <kenniejaydavis@gmail.com>
2012-12-16 00:20:40 -0700

    Rewrite of entryPoint module

--- client/entryPoint.js
+++ client/entryPoint.js
@@ -12,35 +12,34 @@ define('dispatcher', ['underscore', 'backbone'], function(_, backbone) {
 //
 // commands - instantiates all command maps
 
-define([
-		'jquery',
-		'./src/views/main',
-		'./src/commands/documentType',
-		'./src/commands/template',
-		'EventBus'
 define('commands', [
 	'./src/commands/application', 
 	'./src/commands/shell', 
 	'./src/commands/documentManagement',
 	'./src/commands/templateManagement'
 	], 
-	function($, MainViewModel, DocumentTypeCommandMap, TemplateCommandMap, eventBus) {
-		$(function() {
-			var documentTypeMap = new DocumentTypeCommandMap(eventBus),
-			templateMap = new TemplateCommandMap(eventBus);
-
-			MainViewModel.start($('body'));
-
-			eventBus.trigger('loadDocumentTypes');
-			eventBus.trigger('loadTemplates');
 	function (ApplicationCommands, ShellCommands, DocumentManagementCommands, TemplateManagementCommands) {
 		var dispatcher = require('dispatcher');
 
-			console.log('Application started');
-		});
 		var applicationCommands = new ApplicationCommands(dispatcher);
 		var shellCommands = new ShellCommands(dispatcher);
 		var documentManagementCommands = new DocumentManagementCommands(dispatcher);
 		var templateManagementCommands = new TemplateManagementCommands(dispatcher);
 	}
 );
+
+//
+// entryPoint - loads all modules required to start the application
+
+define(['jquery', 'backbone', 'dispatcher', 'commands'], function($, backbone, dispatcher) {
+	var contexts = [];
+	var commands = [];
+
+	$(function () {
+		dispatcher.trigger('application:start', { 'element': $('body').get(0) });
+
+		if (backbone.History.started) {
+			backbone.History.start();
+		}
+	});
+});
