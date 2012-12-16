c4d6a6b95edd31440ece1185bf9a4c3f82355fda
Kennie Davis <kenniejaydavis@gmail.com>
2012-12-16 00:16:31 -0700

    Removed the ApplicationContext definition from the entryPoint

--- client/entryPoint.js
+++ client/entryPoint.js
@@ -6,19 +6,6 @@ define('EventBus', ['underscore', 'backbone'], function(_, backbone) {
 	return eventBus;
 });
 
-define('ApplicationContext', ['backbone', './src/model'], function(backbone, model) {
-	var ApplicationContext = backbone.Model.extend({
-		initialize: function() {
-			//TODO: localize, move to view
-			var defaultDocumentType = new model.DocumentType({name: 'New Document Type'}),
-				defaultTemplate = new model.Template({name: 'New Template'});
-
-			this.set('documentType', defaultDocumentType);
-			this.set('documentTypes', new model.DocumentTypeCollection([defaultDocumentType]));
-			this.set('template', defaultTemplate);
-			this.set('templates', new model.TemplateCollection([defaultTemplate]));
-		}
-	});
 
 	return new ApplicationContext();
 });
