50c6fb0c768ae0afe67b9f0da8ebf84109198761
Steve McGuire <steveorsomethin@gmail.com>
2012-12-16 00:46:16 -0700

    Added page routes and simplified all other routes

--- server/resources/http.js
+++ server/resources/http.js
@@ -84,90 +84,124 @@ httpResources.initialize = function(port) {
 	var app = express.createServer(),
 		documentTypeManager = new domain.DocumentTypeManager(),
 		documentManager = new domain.DocumentManager(),
-		templateManager = new domain.TemplateManager();
+		templateManager = new domain.TemplateManager(),
+		pageManager = new domain.PageManager();
 
 	app.use('/public', express.static(__dirname + '/../../client'));
+
+	app.use(function(err, req, res, next) {
+		res.status(INTERNAL_ERROR).json({ error: err });
+	});
 
 	//TODO: Switch out/augment bodyParser to work with content types other than JSON
 	app.use(express.bodyParser());
 
-	//DocumentTypes
-	var documentTypeRoute = '/documentTypes/:documentType';
+	var serviceBaseRoute = '/services';
 
-	app.put(documentTypeRoute, function(req, res) {
-		documentTypeManager.create(req.params.documentType, req.body, putHandler(res));
+	//DocumentTypes
+	var documentTypeBaseRoute = serviceBaseRoute + '/documentTypes',
+		documentTypeParamRoute = documentTypeBaseRoute + '/:name';
+
+	app.put(documentTypeBaseRoute, function(req, res) {
+		documentTypeManager.create(req.body, putHandler(res));
 	});	
 
-	app.get(documentTypeRoute, function(req, res) {
-		documentTypeManager.read(req.params.documentType, getHandler(res));
+	app.get(documentTypeParamRoute, function(req, res) {
+		documentTypeManager.read(req.params.name, getHandler(res));
 	});
 
-	app.post(documentTypeRoute, function(req, res) {
-		documentTypeManager.update(req.params.documentType, req.body, postHandler(res));
+	app.post(documentTypeBaseRoute, function(req, res) {
+		documentTypeManager.update(req.body, postHandler(res));
 	});
 
-	app.del(documentTypeRoute, function(req, res) {
-		documentTypeManager.del(req.params.documentType, deleteHandler(res));
+	app.del(documentTypeParamRoute, function(req, res) {
+		documentTypeManager.del(req.params.name, deleteHandler(res));
 	});
 
-	app.get('/documentTypes', function(req, res) {
+	app.get(documentTypeBaseRoute, function(req, res) {
 		documentTypeManager.readAll(getHandler(res));
 	});
 
 	//Documents
-	var documentRoute = '/documentTypes/:documentType/documents/:document';
+	var documentBaseRoute = serviceBaseRoute + '/documents',
+		documentParamRoute = documentBaseRoute + '/:name';
 
-	app.put(documentRoute, function(req, res) {
-		documentManager.create(req.params.documentType, req.params.document, req.body, putHandler(res));
+	app.put(documentBaseRoute, function(req, res) {
+		documentManager.create(req.body, putHandler(res));
 	});
 
-	app.get(documentRoute, function(req, res) {
-		documentManager.read(req.params.documentType, req.params.document, getHandler(res));
+	app.get(documentParamRoute, function(req, res) {
+		documentManager.read(req.params.name, getHandler(res));
 	});
 
-	app.post(documentRoute, function(req, res) {
-		documentManager.update(req.params.documentType, req.params.document, req.body, postHandler(res));
+	app.post(documentBaseRoute, function(req, res) {
+		documentManager.update(req.body, postHandler(res));
 	});
 
-	app.del(documentRoute, function(req, res) {
-		documentManager.del(req.params.documentType, req.params.document, deleteHandler(res));
+	app.del(documentParamRoute, function(req, res) {
+		documentManager.del(req.params.name, deleteHandler(res));
 	});
 
-	app.get('/documents', function(req, res) {
+	app.get(documentBaseRoute, function(req, res) {
 		documentManager.filter(req.query.filter, req.query.tag, getHandler(res));
 	});
 
 	//Templates
-	var templateRoute = '/documentTypes/:documentType/templates/:template';
+	var templateBaseRoute = serviceBaseRoute + '/templates',
+		templateParamRoute = templateBaseRoute + '/:name';
 
-	app.put(templateRoute, function(req, res) {
-		templateManager.create(req.params.documentType, req.params.template, req.body, putHandler(res));
+	app.put(templateBaseRoute, function(req, res) {
+		templateManager.create(req.body, putHandler(res));
 	});
 
-	app.get(templateRoute, function(req, res) {
-		templateManager.read(req.params.documentType, req.params.template, getHandler(res));
+	app.get(templateParamRoute, function(req, res) {
+		templateManager.read(req.params.name, getHandler(res));
 	});
 
-	app.post(templateRoute, function(req, res) {
-		templateManager.update(req.params.documentType, req.params.template, req.body, postHandler(res));
+	app.post(templateBaseRoute, function(req, res) {
+		templateManager.update(req.body, postHandler(res));
 	});
 
-	app.del(templateRoute, function(req, res) {
-		templateManager.del(req.params.documentType, req.params.template, deleteHandler(res));
+	app.del(templateParamRoute, function(req, res) {
+		templateManager.del(req.params.name, deleteHandler(res));
 	});
 
-	app.get('/templates', function(req, res) {
+	app.get(templateBaseRoute, function(req, res) {
 		templateManager.readAll(getHandler(res));
+	});
+
+	//Pages
+	var pageBaseRoute = serviceBaseRoute + '/pages',
+		pageParamRoute = pageBaseRoute + '/:name';
+
+	app.put(pageBaseRoute, function(req, res) {
+		pageManager.create(req.body, putHandler(res));
+	});
+
+	app.get(pageParamRoute, function(req, res) {
+		pageManager.read(req.params.name, getHandler(res));
+	});
+
+	app.post(pageBaseRoute, function(req, res) {
+		pageManager.update(req.body, postHandler(res));
+	});
+
+	app.del(pageParamRoute, function(req, res) {
+		pageManager.del(req.params.name, deleteHandler(res));
+	});
+
+	app.get(pageBaseRoute, function(req, res) {
+		pageManager.readAll(getHandler(res));
 	});
 
 	app.post('/dt/:name', function(req, res) {
 		var lastProperty, split, propertyName, subProperty;
 		var documentType = 
 			{
-			    "name" : req.body.name,
-			    "type" : "object",
-			    "additionalProperties" : false,
-			    "properties" :{}
+				"name" : req.body.name,
+				"type" : "object",
+				"additionalProperties" : false,
+				"properties" :{}
 			},
 			properties = documentType.properties;
 
