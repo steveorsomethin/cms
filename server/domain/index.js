4fac73b974a38a23c2095ebed11c7e5f44b4ad5c
Steve McGuire <steveorsomethin@gmail.com>
2012-12-22 21:10:37 -0700

    Module accepts an externally configured persistence implementation

--- server/domain/index.js
+++ server/domain/index.js
@@ -7,334 +7,328 @@ var util = require('util'),
 	async = require('async'),
 	errors = require('../errors'),
 	model = require('./model'),
-	validators = model.validators,
 	persistence = require('../persistence'),
-	mongoosePersistence = require('../persistence/mongoose');
+	validators = model.validators;
 
 var documentTypeNotFound = 'DocumentType with name %s not found.',
 	documentNotFound = 'Document with name %s not found.',
 	templateNotFound = 'Template with name %s not found.',
 	pageNotFound = 'Page with name %s not found.';
 
-var documentTypeRepo = new persistence.DocumentTypeRepo(mongoosePersistence.documentTypes),
-	documentRepo = new persistence.DocumentRepo(mongoosePersistence.documents),
-	templateRepo = new persistence.TemplateRepo(mongoosePersistence.templates),
-	pageRepo = new persistence.TemplateRepo(mongoosePersistence.pages);
+var domain = module.exports = function(persistenceImpl) {
+	var domain = {};
 
-var domainManagers = module.exports = {};
+	var documentTypeRepo = new persistence.DocumentTypeRepo(persistenceImpl.documentTypes),
+		documentRepo = new persistence.DocumentRepo(persistenceImpl.documents),
+		templateRepo = new persistence.TemplateRepo(persistenceImpl.templates),
+		pageRepo = new persistence.TemplateRepo(persistenceImpl.pages);
 
-var ensureDocumentType = function(documentTypeName, callback) {
-	documentTypeRepo.read(documentTypeName, function(error, documentType) {
-		if (!documentType) {
-			error = new errors.ResourceNotFound(util.format(documentTypeNotFound, documentTypeName));
-			return callback(error);
-		} else {
-			callback(null, documentType);
-		}
-	});
-};
-
-var ensureTemplate = function(templateName, callback) {
-	templateRepo.read(templateName, function(error, template) {
-		if (!template) {
-			error = new errors.ResourceNotFound(util.format(templateNotFound, templateName));
-			return callback(error);
-		} else {
-			callback(null, template);
-		}
-	});
-};
-
-var preSaveDocument = function(document, callback) {
-	if (typeof document.documentType !== 'string') {
-		return callback(new errors.InvalidInput({documentType: 'Property is required'}));
-	}
-
-	async.waterfall([
-		function(callback) {
-			ensureDocumentType(document.documentType, callback);
-		},
-
-		function(documentType, callback) {
-			document.tags = document.tags || [];
-			var validationError = validators.Document(document, documentType);
-			if (validationError) {
-				return callback(validationError);
+	var ensureDocumentType = function(documentTypeName, callback) {
+		documentTypeRepo.read(documentTypeName, function(error, documentType) {
+			if (!documentType) {
+				error = new errors.ResourceNotFound(util.format(documentTypeNotFound, documentTypeName));
+				return callback(error);
+			} else {
+				callback(null, documentType);
 			}
-
-			return callback();
-		}
-	], callback);
-};
-
-var preSaveTemplate = function(template, callback) {
-	var validationError = validators.Template(template);
-	if (validationError) {
-		return callback(validationError);
-	}
-
-	ensureDocumentType(template.documentType, function(error, result) {
-		return error ? callback(error) : callback();
-	});
-};
-
-var preSavePage = function(page, callback) {
-	var validationError = validators.Page(page);
-	if (validationError) {
-		return callback(validationError);
-	}
-
-	var templateTask = function(templateName) {
-		return function(callback) {
-			ensureTemplate(templateName, callback);
-		};
+		});
 	};
 
-	var documentTypeTask = function(documentTypeName) {
-		return function(callback) {
-			ensureDocumentType(documentTypeName, callback);
-		};
+	var ensureTemplate = function(templateName, callback) {
+		templateRepo.read(templateName, function(error, template) {
+			if (!template) {
+				error = new errors.ResourceNotFound(util.format(templateNotFound, templateName));
+				return callback(error);
+			} else {
+				callback(null, template);
+			}
+		});
 	};
 
-	var tasks = [templateTask(page.layout)];
-	_.each(page.sections, function(section) {
-		tasks.push(templateTask(section.template));
-		tasks.push(documentTypeTask(section.filter.documentType));
-	});
+	var preSaveDocument = function(document, callback) {
+		if (typeof document.documentType !== 'string') {
+			return callback(new errors.InvalidInput({documentType: 'Property is required'}));
+		}
 
-	async.parallel(tasks, function(error, result) {
-		return error ? callback(error) : callback();
-	});
-};
+		async.waterfall([
+			function(callback) {
+				ensureDocumentType(document.documentType, callback);
+			},
 
-//DocumentTypes
-var DocumentTypeManager = domainManagers.DocumentTypeManager = function() {
+			function(documentType, callback) {
+				document.tags = document.tags || [];
+				var validationError = validators.Document(document, documentType);
+				if (validationError) {
+					return callback(validationError);
+				}
 
-};
+				return callback();
+			}
+		], callback);
+	};
 
-DocumentTypeManager.prototype.create = function(documentType, onComplete) {
-	var validationError = validators.DocumentType(documentType);
-	documentType.id = documentType.name;
-	if (validationError) {
-		return onComplete(validationError);
-	} else {
-		return documentTypeRepo.create(documentType, onComplete);
-	}
-};
+	var preSaveTemplate = function(template, callback) {
+		var validationError = validators.Template(template);
+		if (validationError) {
+			return callback(validationError);
+		}
 
-DocumentTypeManager.prototype.read = function(documentTypeName, onComplete) {
-	documentTypeRepo.read(documentTypeName, function(error, result) {
-		if (error) {
-			return onComplete(error);
-		} else if (!result) {
-			error = new errors.ResourceNotFound(util.format(documentTypeNotFound, documentTypeName));
-			return onComplete(error);
+		ensureDocumentType(template.documentType, function(error, result) {
+			return error ? callback(error) : callback();
+		});
+	};
+
+	var preSavePage = function(page, callback) {
+		var validationError = validators.Page(page);
+		if (validationError) {
+			return callback(validationError);
+		}
+
+		var templateTask = function(templateName) {
+			return function(callback) {
+				ensureTemplate(templateName, callback);
+			};
+		};
+
+		var documentTypeTask = function(documentTypeName) {
+			return function(callback) {
+				ensureDocumentType(documentTypeName, callback);
+			};
+		};
+
+		var tasks = [templateTask(page.layout)];
+		_.each(page.sections, function(section) {
+			tasks.push(templateTask(section.template));
+			tasks.push(documentTypeTask(section.filter.documentType));
+		});
+
+		async.parallel(tasks, function(error, result) {
+			return error ? callback(error) : callback();
+		});
+	};
+
+	//Document Types
+	var documentTypes = domain.documentTypes = {};
+
+	documentTypes.create = function(documentType, onComplete) {
+		var validationError = validators.DocumentType(documentType);
+		documentType.id = documentType.name;
+		if (validationError) {
+			return onComplete(validationError);
 		} else {
-			return onComplete(null, result);
+			return documentTypeRepo.create(documentType, onComplete);
 		}
-	});
-};
+	};
 
-DocumentTypeManager.prototype.readAll = function(onComplete) {
-	documentTypeRepo.readAll(function(error, result) {
-		if (error) {
-			return onComplete(error);
-		} else if (!result) {
-			error = new errors.ResourceNotFound(util.format(documentTypeNotFound, 'all'));
-			return onComplete(error);
+	documentTypes.read = function(documentTypeName, onComplete) {
+		documentTypeRepo.read(documentTypeName, function(error, result) {
+			if (error) {
+				return onComplete(error);
+			} else if (!result) {
+				error = new errors.ResourceNotFound(util.format(documentTypeNotFound, documentTypeName));
+				return onComplete(error);
+			} else {
+				return onComplete(null, result);
+			}
+		});
+	};
+
+	documentTypes.readAll = function(onComplete) {
+		documentTypeRepo.readAll(function(error, result) {
+			if (error) {
+				return onComplete(error);
+			} else if (!result) {
+				error = new errors.ResourceNotFound(util.format(documentTypeNotFound, 'all'));
+				return onComplete(error);
+			} else {
+				return onComplete(null, result);
+			}
+		});
+	};
+
+	documentTypes.update = function(documentType, onComplete) {
+		var validationError = validators.DocumentType(documentType);
+		documentType.id = documentType.name;
+		if (validationError) {
+			return onComplete(validationError);
 		} else {
-			return onComplete(null, result);
+			documentTypeRepo.update(documentType, onComplete);
 		}
-	});
-};
+	};
 
-DocumentTypeManager.prototype.update = function(documentType, onComplete) {
-	var validationError = validators.DocumentType(documentType);
-	documentType.id = documentType.name;
-	if (validationError) {
-		return onComplete(validationError);
-	} else {
-		documentTypeRepo.update(documentType, onComplete);
-	}
-};
+	documentTypes.del = function(documentTypeName, onComplete) {
+		documentTypeRepo.del(documentTypeName, onComplete);
+	};
 
-DocumentTypeManager.prototype.del = function(documentTypeName, onComplete) {
-	documentTypeRepo.del(documentTypeName, onComplete);
-};
+	//Documents
+	var documents = domain.documents = {};
 
-//Documents
-var DocumentManager = domainManagers.DocumentManager = function() {
-	//Whatever here
-};
+	documents.create = function(document, onComplete) {
+		async.waterfall([
+			function(callback) {
+				preSaveDocument(document, callback);
+			},
 
-DocumentManager.prototype.create = function(document, onComplete) {
-	async.waterfall([
-		function(callback) {
-			preSaveDocument(document, callback);
-		},
+			function(callback) {
+				documentRepo.create(document, callback);
+			}
+		], onComplete);
+	};
 
-		function(callback) {
-			documentRepo.create(document, callback);
-		}
-	], onComplete);
-};
+	documents.read = function(documentName, onComplete) {
+		documentRepo.read(documentName, function(error, result) {
+			if (error) {
+				return onComplete(error);
+			} else if (!result) {
+				error = new errors.ResourceNotFound(util.format(documentNotFound, documentName));
+				return onComplete(error);
+			} else {
+				return onComplete(null, result);
+			}
+		});
+	};
 
-DocumentManager.prototype.read = function(documentName, onComplete) {
-	documentRepo.read(documentName, function(error, result) {
-		if (error) {
-			return onComplete(error);
-		} else if (!result) {
-			error = new errors.ResourceNotFound(util.format(documentNotFound, documentName));
-			return onComplete(error);
-		} else {
-			return onComplete(null, result);
-		}
-	});
-};
+	documents.filter = function(filter, onComplete) {
+		documentRepo.filter(filter, onComplete); //TODO: Fill this out
+	};
 
-DocumentManager.prototype.filter = function(filter, onComplete) {
-	documentRepo.filter(filter, onComplete); //TODO: Fill this out
-};
+	documents.update = function(document, onComplete) {
+		async.waterfall([
+			function(callback) {
+				preSaveDocument(document, callback);
+			},
 
-DocumentManager.prototype.update = function(document, onComplete) {
-	async.waterfall([
-		function(callback) {
-			preSaveDocument(document, callback);
-		},
+			function(callback) {
+				documentRepo.update(document, callback);
+			}
+		], onComplete);
+	};
 
-		function(callback) {
-			documentRepo.update(document, callback);
-		}
-	], onComplete);
-};
+	documents.del = function(documentName, onComplete) {
+		documentRepo.del(documentName, onComplete);
+	};
 
-DocumentManager.prototype.del = function(documentName, onComplete) {
-	documentRepo.del(documentName, onComplete);
-};
+	//Templates
+	var templates = domain.templates = {};
 
-//Templates
-var TemplateManager = domainManagers.TemplateManager = function() {
-	//Whatever here
-};
+	templates.create = function(template, onComplete) {
+		async.waterfall([
+			function(callback) {
+				preSaveTemplate(template, callback);
+			},
 
-TemplateManager.prototype.create = function(template, onComplete) {
-	async.waterfall([
-		function(callback) {
-			preSaveTemplate(template, callback);
-		},
+			function(callback) {
+				templateRepo.create(template, callback);
+			}
+		], onComplete);
+	};
 
-		function(callback) {
-			templateRepo.create(template, callback);
-		}
-	], onComplete);
-};
+	templates.filter = function(filter, onComplete) {
+		templateRepo.filter(filter, onComplete); //TODO: Fill this out
+	};
 
-TemplateManager.prototype.filter = function(filter, onComplete) {
-	templateRepo.filter(filter, onComplete); //TODO: Fill this out
-};
+	templates.read = function(templateName, onComplete) {
+		templateRepo.read(templateName, function(error, result) {
+			if (error) {
+				return onComplete(error);
+			} else if (!result) {
+				error = new errors.ResourceNotFound(util.format(templateNotFound, templateName));
+				return onComplete(error);
+			} else {
+				return onComplete(null, result);
+			}
+		});
+	};
 
-TemplateManager.prototype.read = function(templateName, onComplete) {
-	templateRepo.read(templateName, function(error, result) {
-		if (error) {
-			return onComplete(error);
-		} else if (!result) {
-			error = new errors.ResourceNotFound(util.format(templateNotFound, templateName));
-			return onComplete(error);
-		} else {
-			return onComplete(null, result);
-		}
-	});
-};
+	templates.readAll = function(onComplete) {
+		templateRepo.readAll(function(error, result) {
+			if (error) {
+				return onComplete(error);
+			} else if (!result) {
+				error = new errors.ResourceNotFound(util.format(templateNotFound, 'all'));
+				return onComplete(error);
+			} else {
+				return onComplete(null, result);
+			}
+		});
+	};
 
+	templates.update = function(template, onComplete) {
+		async.waterfall([
+			function(callback) {
+				preSaveTemplate(template, callback);
+			},
 
-TemplateManager.prototype.readAll = function(onComplete) {
-	templateRepo.readAll(function(error, result) {
-		if (error) {
-			return onComplete(error);
-		} else if (!result) {
-			error = new errors.ResourceNotFound(util.format(templateNotFound, 'all'));
-			return onComplete(error);
-		} else {
-			return onComplete(null, result);
-		}
-	});
-};
+			function(callback) {
+				templateRepo.update(template, callback);
+			}
+		], onComplete);
+	};
 
-TemplateManager.prototype.update = function(template, onComplete) {
-	async.waterfall([
-		function(callback) {
-			preSaveTemplate(template, callback);
-		},
+	templates.del = function(templateName, onComplete) {
+		templateRepo.del(templateName, onComplete);
+	};
 
-		function(callback) {
-			templateRepo.update(template, callback);
-		}
-	], onComplete);
-};
+	//Pages
+	var pages = domain.pages = {};
 
-TemplateManager.prototype.del = function(templateName, onComplete) {
-	templateRepo.del(templateName, onComplete);
-};
+	pages.create = function(page, onComplete) {
+		async.waterfall([
+			function(callback) {
+				preSavePage(page, callback);
+			},
 
-//Pages
-var PageManager = domainManagers.PageManager = function() {
-	//Whatever here
-};
+			function(callback) {
+				pageRepo.create(page, callback);
+			}
+		], onComplete);
+	};
 
-PageManager.prototype.create = function(page, onComplete) {
-	async.waterfall([
-		function(callback) {
-			preSavePage(page, callback);
-		},
+	pages.filter = function(filter, onComplete) {
+		pageRepo.filter(filter, onComplete); //TODO: Fill this out
+	};
 
-		function(callback) {
-			pageRepo.create(page, callback);
-		}
-	], onComplete);
-};
+	pages.read = function(pageName, onComplete) {
+		pageRepo.read(pageName, function(error, result) {
+			if (error) {
+				return onComplete(error);
+			} else if (!result) {
+				error = new errors.ResourceNotFound(util.format(pageNotFound, pageName));
+				return onComplete(error);
+			} else {
+				return onComplete(null, result);
+			}
+		});
+	};
 
-PageManager.prototype.filter = function(filter, onComplete) {
-	pageRepo.filter(filter, onComplete); //TODO: Fill this out
-};
+	pages.readAll = function(onComplete) {
+		pageRepo.readAll(function(error, result) {
+			if (error) {
+				return onComplete(error);
+			} else if (!result) {
+				error = new errors.ResourceNotFound(util.format(pageNotFound, 'all'));
+				return onComplete(error);
+			} else {
+				return onComplete(null, result);
+			}
+		});
+	};
 
-PageManager.prototype.read = function(pageName, onComplete) {
-	pageRepo.read(pageName, function(error, result) {
-		if (error) {
-			return onComplete(error);
-		} else if (!result) {
-			error = new errors.ResourceNotFound(util.format(pageNotFound, pageName));
-			return onComplete(error);
-		} else {
-			return onComplete(null, result);
-		}
-	});
-};
+	pages.update = function(page, onComplete) {
+		async.waterfall([
+			function(callback) {
+				preSavePage(page, callback);
+			},
 
-PageManager.prototype.readAll = function(onComplete) {
-	pageRepo.readAll(function(error, result) {
-		if (error) {
-			return onComplete(error);
-		} else if (!result) {
-			error = new errors.ResourceNotFound(util.format(pageNotFound, 'all'));
-			return onComplete(error);
-		} else {
-			return onComplete(null, result);
-		}
-	});
-};
+			function(callback) {
+				pageRepo.update(page, callback);
+			}
+		], onComplete);
+	};
 
-PageManager.prototype.update = function(page, onComplete) {
-	async.waterfall([
-		function(callback) {
-			preSavePage(page, callback);
-		},
+	pages.del = function(pageName, onComplete) {
+		pageRepo.del(pageName, onComplete);
+	};
 
-		function(callback) {
-			pageRepo.update(page, callback);
-		}
-	], onComplete);
-};
-
-PageManager.prototype.del = function(pageName, onComplete) {
-	pageRepo.del(pageName, onComplete);
+	return domain;
 };
