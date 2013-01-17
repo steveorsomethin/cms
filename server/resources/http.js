e6e440b854ea6462d29f1230838af7e5ad091a62
Kennie Davis <kenniejaydavis@gmail.com>
2013-01-16 19:36:18 -0700

    Merged from Master

--- server/resources/http.js
+++ server/resources/http.js
@@ -2,7 +2,6 @@
 
 var util = require('util'),
 	express = require('express'),
-	domain = require('../domain'),
 	errors = require('../errors');
 
 var httpResources = module.exports = {};
@@ -38,50 +37,23 @@ var errorMap = (function() {
 
 var sendError = function(error, response) {
 	var status = error.type && errorMap[error.type] ? errorMap[error.type] : 500;
-	//console.error(JSON.stringify(error));
 	response.status(status).json(error);
 };
 
 //TODO: Blow this out to inspect Accept headers and serialize appropriately
-var sendResponse = function(error, body, successCode, response) {
-	if (error) {
-		return sendError(error, response);
-	} else {
-		return response.status(successCode).json(body);
-	}
+var sendResponse = function(successCode, response) {
+	return function(error, result) {
+		if (error) {
+			return sendError(error, response);
+		} else {
+			return response.status(successCode).json(result);
+		}
+	};
 };
 
-var 
-	putHandler = function(response) {
-		return function(error, result) {
-			sendResponse(error, result, CREATED, response);
-		};
-	},
-
-	getHandler = function(response) {
-		return function(error, result) {
-			sendResponse(error, result, OK, response);
-		};
-	},
-
-	postHandler = function(response) {
-		return function(error, result) {
-			sendResponse(error, result, ACCEPTED, response);
-		};
-	},
-
-	deleteHandler = function(response) {
-		return function(error, result) {
-			sendResponse(error, result, NO_CONTENT, response);
-		};
-	};
-
-httpResources.initialize = function(port) {
+httpResources.initialize = function(config) {
 	var app = express.createServer(),
-		documentTypeManager = new domain.DocumentTypeManager(),
-		documentManager = new domain.DocumentManager(),
-		templateManager = new domain.TemplateManager(),
-		pageManager = new domain.PageManager();
+		domain = config.domain;
 
 	app.use('/public', express.static(__dirname + '/../../client'));
 
@@ -94,114 +66,75 @@ httpResources.initialize = function(port) {
 
 	var serviceBaseRoute = '/services';
 
+	var crud = function(manager) {
+		return function(req, res) {
+			switch (req.method) {
+				case 'PUT':
+					manager.create(req.body, sendResponse(CREATED, res));
+					break;
+				case 'GET':
+					manager.read(req.params.name, sendResponse(OK, res));
+					break;
+				case 'POST':
+					manager.update(req.body, sendResponse(ACCEPTED, res));
+					break;
+				case 'DELETE':
+					manager.del(req.params.name, sendResponse(NO_CONTENT, res));
+					break;
+			}
+		};
+	};
+
+	//Set up boilerplate crud routes
+	[
+		['documentTypes', domain.documentTypes], 
+		['documents', domain.documents],
+		['templates', domain.templates],
+		['pages', domain.pages]
+	].forEach(function(tuple, i) {
+		var baseRoute = util.format('%s/%s', serviceBaseRoute, tuple[0]),
+			paramRoute = util.format('%s/:name', baseRoute),
+			manager = tuple[1];
+
+		app.put(baseRoute, crud(manager));
+		app.get(paramRoute, crud(manager));
+		app.post(baseRoute, crud(manager));
+		app.del(paramRoute, crud(manager));
+	});
+
 	//DocumentTypes
-	var documentTypeBaseRoute = serviceBaseRoute + '/documentTypes',
-		documentTypeParamRoute = documentTypeBaseRoute + '/:name';
-
-	app.put(documentTypeBaseRoute, function(req, res) {
-		documentTypeManager.create(req.body, putHandler(res));
-	});	
-
-	app.get(documentTypeParamRoute, function(req, res) {
-		documentTypeManager.read(req.params.name, getHandler(res));
-	});
-
-	app.post(documentTypeBaseRoute, function(req, res) {
-		documentTypeManager.update(req.body, postHandler(res));
-	});
-
-	app.del(documentTypeParamRoute, function(req, res) {
-		documentTypeManager.del(req.params.name, deleteHandler(res));
-	});
-
-	app.get(documentTypeBaseRoute, function(req, res) {
-		documentTypeManager.readAll(getHandler(res));
+	app.get(serviceBaseRoute + '/documentTypes', function(req, res) {
+		domain.documentTypes.readAll(sendResponse(OK, res));
 	});
 
 	//Documents
-	var documentBaseRoute = serviceBaseRoute + '/documents',
-		documentParamRoute = documentBaseRoute + '/:name';
-
-	app.put(documentBaseRoute, function(req, res) {
-		documentManager.create(req.body, putHandler(res));
-	});
-
-	app.get(documentParamRoute, function(req, res) {
-		documentManager.read(req.params.name, getHandler(res));
-	});
-
-	app.post(documentBaseRoute, function(req, res) {
-		documentManager.update(req.body, postHandler(res));
-	});
-
-	app.del(documentParamRoute, function(req, res) {
-		documentManager.del(req.params.name, deleteHandler(res));
-	});
-
-	app.get(documentBaseRoute, function(req, res) {
-		documentManager.filter(req.query.filter, req.query.tag, getHandler(res));
+	app.get(serviceBaseRoute + '/documents', function(req, res) {
+		domain.documents.filter(null, sendResponse(OK, res));
 	});
 
 	//Templates
-	var templateBaseRoute = serviceBaseRoute + '/templates',
-		templateParamRoute = templateBaseRoute + '/:name';
-
-	app.put(templateBaseRoute, function(req, res) {
-		templateManager.create(req.body, putHandler(res));
-	});
-
-	app.get(templateParamRoute, function(req, res) {
-		templateManager.read(req.params.name, getHandler(res));
-	});
-
-	app.post(templateBaseRoute, function(req, res) {
-		templateManager.update(req.body, postHandler(res));
-	});
-
-	app.del(templateParamRoute, function(req, res) {
-		templateManager.del(req.params.name, deleteHandler(res));
-	});
-
-	app.get(templateBaseRoute, function(req, res) {
-		templateManager.readAll(getHandler(res));
+	app.get(serviceBaseRoute + '/templates', function(req, res) {
+		//TODO: Hack, we need a generalized filter ability
+		var filter = req.query.isLayout === 'true' ? {'template.isLayout': true} : {};
+		domain.templates.filter(filter, sendResponse(OK, res));
 	});
 
 	//Pages
-	var pageBaseRoute = serviceBaseRoute + '/pages',
-		pageParamRoute = pageBaseRoute + '/:name';
-
-	app.put(pageBaseRoute, function(req, res) {
-		pageManager.create(req.body, putHandler(res));
+	app.get(serviceBaseRoute + '/pages', function(req, res) {
+		domain.pages.readAll(sendResponse(OK, res));
 	});
 
-	app.get(pageParamRoute, function(req, res) {
-		pageManager.read(req.params.name, getHandler(res));
-	});
-
-	app.post(pageBaseRoute, function(req, res) {
-		pageManager.update(req.body, postHandler(res));
-	});
-
-	app.del(pageParamRoute, function(req, res) {
-		pageManager.del(req.params.name, deleteHandler(res));
-	});
-
-	app.get(pageBaseRoute, function(req, res) {
-		pageManager.readAll(getHandler(res));
-	});
-
-	app.get(pageParamRoute + '/render', function(req, res) {
+	app.get(serviceBaseRoute + '/pages/:name/render', function(req, res) {
 		var async = require('async'),
-			_ = require('underscore'),
 			dust = require('dustjs-linkedin');
 		async.waterfall([
 			function(callback) {
-				pageManager.read(req.params.name, callback);
+				domain.pages.read(req.params.name, callback);
 			},
 			function(page, callback) {
 				var tasks = [
 					function(callback) {
-						templateManager.read(page.layout, function(error, result) {
+						domain.templates.read(page.layout, function(error, result) {
 							if (error) {
 								return callback(error);
 							} else {
@@ -211,9 +144,9 @@ httpResources.initialize = function(port) {
 						});
 					}
 				];
-				_.each(page.sections, function(section) {
+				page.sections.forEach(function(section) {
 					tasks.push(function(callback) {
-						templateManager.read(section.template, function(error, result) {
+						domain.templates.read(section.template, function(error, result) {
 							if (error) {
 								return callback(error);
 							} else {
@@ -224,11 +157,11 @@ httpResources.initialize = function(port) {
 					});
 					tasks.push(function(callback) {
 						section.filter.parameters = req.query;
-						documentManager.filter(section.filter, function(error, result) {
+						domain.documents.filter(section.filter, function(error, result) {
 							if (error) {
 								return callback(error);
 							} else {
-								section.document = result.body;
+								section.document = result;
 								callback();
 							}
 						});
@@ -250,8 +183,8 @@ httpResources.initialize = function(port) {
 			}
 
 			var page = result, document = {}, compiled;
-			_.each(page.sections, function(section) {
-				document[section.placeHolder] = section.document;
+			page.sections.forEach(function(section) {
+				document[section.placeHolder] = section.document.body;
 				compiled = dust.compile(section.template.body, section.placeHolder);
 				dust.loadSource(compiled);
 			});
@@ -268,5 +201,7 @@ httpResources.initialize = function(port) {
 		});
 	});
 
-	app.listen(port);
+	app.listen(config.port);
+
+	return app;
 };
